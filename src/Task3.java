2863a6c22c68307e7807ac65c9b5623299471386
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-11-20 20:23:50 +0300

    Add task-3

---
+++ src/Task3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Enter a, b, h: ");
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        double h = scan.nextDouble();
+        while (a <= b){
+            System.out.println(a + " " + solveFunction(a));
+            a += h;
+        }
+    }
+    public static double solveFunction(double x){
+        return Math.tan(x);
+    }
+}
