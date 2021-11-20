e6de5749cd25238466ac9a33e01bb55a0c9d13e7
Roman Polishchuk <rmn.polishchuk@gmail.com>
2021-11-20 21:08:19 +0300

    Add task-6

---
+++ src/Task6.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите N: ");
+        int N = in.nextInt();
+        System.out.print("Введите " + N + " элементов: ");
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = in.nextInt();
+        }
+        int[][] matrix = createMatrix(array);
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] createMatrix(int[] array){
+        int length = array.length;;
+        int[][] matrix = new int[length][length];
+
+        for(int i = 0; i < length; i++){
+            for(int j = 0; j < length; j++){
+                matrix[i][j] = array[(i+j)% length];
+            }
+        }
+        return matrix;
+    }
+}
